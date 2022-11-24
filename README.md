aa188f8104021a9ac80b696998643ab3d4728e55
Alfiya <simalfiya321@gmail.com>
2022-11-24 17:39:21 +0530

    Completed Todo Application

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1>Todo Application</h1>
+
+### Hii **Alfiyabanu Bhojani** here
+
+This is my Todo Application which is made up from React Library...
+
+In which I have used State, Filter, Map and etc Method to complete it..
+
+**Time taken**:-
+
+**5 hours**
+
+**Technologies used**:-
+
+**- React**
+
+**Output**:-
+
+![](./image/image1.png)
+![](./image/image2.png)
+![](./image/image3.png)
---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import "./style.css";
+import { v1 as uuidv1 } from "uuid";
+
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [completed, setCompleted] = useState([]);
+
+  const addTodo = () => {
+    const updatedTodo = [{ todoText: inputValue, id: uuidv1() }, ...todos];
+    setTodos(updatedTodo);
+    setInputValue("");
+  };
+
+  const deleteTodo = (id) => {
+    const filteredTodos = todos.filter((i) => i.id !== id);
+    setTodos(filteredTodos);
+  };
+
+  const checkTodo = (id) => {
+    if (completed.includes(id)) {
+      setCompleted(completed.filter((c) => c !== id));
+    } else {
+      setCompleted([...completed, id]);
+    }
+  };
+
+  // console.log(completed);
+
+  return (
+    <div className="todo__container">
+      <h1>TODO APPLICATION</h1>
+      <section className="input__container">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+        />
+        <button onClick={addTodo}>ADD TASK</button>
+      </section>
+      <section className="list__container">
+        {todos.map((v) => {
+          // console.log(v);
+          return (
+            <div
+              className={`list__item ${
+                completed.includes(v.id) ? "checked__list__item" : ""
+              }`}
+              key={v.id}
+            >
+              <p>{v.todoText}</p>
+              <section>
+                <button
+                  className="cm_btn danger"
+                  onClick={() => {
+                    deleteTodo(v.id);
+                  }}
+                >
+                  DELETE
+                </button>
+                <button
+                  className="cm_btn check"
+                  onClick={() => {
+                    checkTodo(v.id);
+                  }}
+                >
+                  DONE
+                </button>
+              </section>
+            </div>
+          );
+        })}
+      </section>
+    </div>
+  );
+};
+
+export default App;
